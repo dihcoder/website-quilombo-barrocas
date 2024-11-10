68b6a999f9f63de506e415860ba8acc0c0e4dd76
diegobyter <mdsidiego@gmail.com>
2024-11-09 22:46:11 -0300

    Implements interactive modal

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-const $dropupBtn = document.getElementById('btnLanguageSwitcher');
-const $langChoiceBtns = document.querySelectorAll('.dropdown-item.language-choice');
-
 function detectLanguage() {
     const lang = navigator.language || navigator.userLanguage;
     return lang.startsWith('pt') ? 'pt' : 'en';
@@ -13,21 +10,35 @@ function loadLanguageFile(language) {
 }
 
 async function switchLanguage(language) {
+    if ($('html').attr('lang') === language)
+        return false;
+
+    $('html').attr('lang', language)
+
     const content = await loadLanguageFile(language);
+
+    localStorage.setItem('preferredLanguage', language);
+
     for (const [key, value] of Object.entries(content)) {
         const htmlTag = document.getElementById(key);
         if (htmlTag) htmlTag.textContent = value;
     }
 }
 
-$langChoiceBtns.forEach(btn => btn.addEventListener('click', function (e) {
-    e.preventDefault
+function removeLanguagePreference() {
+    const modalBeforeChanges = $('#staticBackdrop .modal-content').html();
 
-    $dropupBtn.textContent = btn.textContent;
-    const language = btn.dataset.lang;
-    switchLanguage(language);
-    localStorage.setItem('preferredLanguage', language);
-}))
+    localStorage.removeItem('preferredLanguage');
+
+    $('.modal-footer').html("");
+
+    $('.modal-body').html(`<img src="./images/icons/check.gif" class="img-fluid mx-auto d-block" width="100px" alt="">
+            <p class="text-center">Removido!</p>`)
+
+    setTimeout(() => $('#staticBackdrop').modal('hide'), 1800)
+
+    setTimeout(() => $('#staticBackdrop .modal-content').html(modalBeforeChanges), 2000)
+}
 
 const userPreferredLanguage = localStorage.getItem('preferredLanguage') || detectLanguage();
 switchLanguage(userPreferredLanguage);
